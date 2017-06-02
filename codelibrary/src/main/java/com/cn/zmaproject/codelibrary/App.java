761d72ec53295240b50bb07ad1e981230f955c28
zhangmingan <549669699@qq.com>
2017-06-02 14:01:35 +0800

    2017.6.2 first bluid

---
+++ codelibrary/src/main/java/com/cn/zmaproject/codelibrary/App.java
@@ -0,0 +1,13 @@
+package com.cn.zmaproject.codelibrary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
